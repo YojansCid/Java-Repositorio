693f499e13394b92b8ff7c2372b998d9b95082e7
Yojans Cid <ycid@alumnos.ubiobio.cl>
2017-09-12 04:37:14 -0700

    arreglo de comentarios

--- 19 (metodos de instancia y metodos de clase)/src/ejemplo19.java
+++ 19 (metodos de instancia y metodos de clase)/src/ejemplo19.java
@@ -14,10 +14,16 @@ public class ejemplo19 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+/*Instancia del objeto Animal*/        
         Animal a = new Animal(4, "perro","shopper");
-        
+
+/*Metodo parte del objeto recien instanciado, solo se puede acceder a ese 
+ * metodo a travez de una instancia de un objeto*/        
         a.rugirDeInstancia();
-        
+
+/*Metodo de clase, que no necesita una instancia de un objeto, ya que es de 
+ * tipo STATIC el metodo se puede usar con el nombre de la clase seguida de un 
+ * punto y el nombre del metodo estatico (Animal.rugirDeClase())*/        
         Animal.rugirDeClase();
     }
     
